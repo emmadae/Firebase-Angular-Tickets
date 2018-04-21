31a5bb55f3d70fe3d11aa3744cced8c0a264f556
emmadae <emmadae@gmail.com>
2018-04-21 10:15:16 -0500

    firebase update config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyABKjjh0U0xJhKpVxbrdN4PeH9v48vGow8",
+    authDomain: "ticket-counter.firebaseapp.com",
+    databaseURL: "https://ticket-counter.firebaseio.com",
+    projectId: "ticket-counter",
+    storageBucket: "",
+    messagingSenderId: "858958839607"
+  }
 };
